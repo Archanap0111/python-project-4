385ec0c9aef57b8ee1a434262419ba6ca6174d50
Archanap0111 <archanaprasad0111@gmail.com>
2025-04-07 14:17:28 +0530

    added new practice to prthon

---
+++ demo.py
@@ -0,0 +1,20 @@
+#day 1
+
+name="prasad "            
+print(name)  
+  
+#day2
+cup="tea"
+
+print()
+
+glass = "water"
+print(glass)
+
+a=10
+b=20
+print(a)
+print(b)
+a,b=b,a 
+print(a)
+print(b)
